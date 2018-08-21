96b37bf49dc7110b8cddcc995f8cb1aefe8523b3
Melvin Joseph <32485035+melvinJoseph96@users.noreply.github.com>
2018-08-21 16:18:22 +0100

    web application foundation

---
+++ WebApplication1MVC/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1MVC.Models
+{
+    public class PersonModel
+    {
+
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+
+
+
+    }
+}
